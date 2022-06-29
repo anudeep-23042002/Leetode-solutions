19e6974c402dc9de7426bca871f286fd957b5178
anudeep-23042002 <88811397+anudeep-23042002@users.noreply.github.com>
2022-06-29 11:17:31 +0530

    Time: 6 ms (27.26%), Space: 6.2 MB (44.89%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        vector<int> dp(s.size()+1);
-        dp[0]=1;
-        if(s[0]=='0')dp[1]=0;
-        else dp[1]=1;
-        for(int i=2;i<=s.size();i++){
-            int way1,way2;
-            if(s[i-1]=='0')way1=0;
-            else way1=dp[i-1];
-            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
-            else way2=0;
-            dp[i]=way1+way2;
+    int numDecodings(string A) {
+        int n=A.size();
+    vector<int>dp(n+1,0);
+    dp[0]=1;
+    if(A[0]=='0')
+        dp[1]=0;
+    else {
+        dp[1]=1;
+    }
+    for(int i=2;i<=n;i++){
+        int a=0,b=0;
+        if(A[i-1]!='0'){
+            b=dp[i-1];
         }
-        return dp[s.size()];
+        string c="";
+        for(int j=i-2;j<i;j++){
+            c=c+A[j];
+        }
+        if(stoi(c)<=26 && stoi(c)>0 && A[i-2]!='0'){
+            a=dp[i-2];
+        }
+        dp[i]=a+b;
+    }
+    return dp[n];
     }
 };
