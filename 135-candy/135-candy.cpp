eece487340d781e06a05da3598ed90827b2314e6
anudeep-23042002 <88811397+anudeep-23042002@users.noreply.github.com>
2022-07-27 10:00:08 +0530

    Time: 147 ms (5.04%), Space: 17.8 MB (59.78%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int candy(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> candy(n,1);
-        for(int i=1;i<n;i++){
-            if(arr[i-1] < arr[i]){
-                candy[i] = candy[i-1] + 1;
-            }
-        }
+        int n=arr.size();
+        vector<int>v(n,1);
         for(int i=n-2;i>=0;i--){
-            if(arr[i] > arr[i+1]){
-                candy[i] = max(candy[i],candy[i+1] + 1);
+            if(arr[i]>arr[i+1]){
+                v[i]=v[i+1]+1;
             }
         }
-        int ans = accumulate(candy.begin(),candy.end(),0);
-        return ans;
+        for(int i=1;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                v[i]=max(v[i-1]+1,v[i]);
+            }
+        }
+        for(auto i:v){
+            cout<<i<<" ";
+        }
+        return accumulate(v.begin(),v.end(),0);
     }
 };
